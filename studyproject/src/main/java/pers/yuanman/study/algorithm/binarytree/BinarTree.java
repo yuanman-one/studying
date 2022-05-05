cf0e3e45e01fdd6d13bdef02fcfa240131df254a
lthink <3372174277@qq.com>
2022-05-05 19:34:09 +0800

    *修改二叉排序树

--- studyproject/src/main/java/pers/yuanman/study/algorithm/binarytree/BinarTree.java
+++ studyproject/src/main/java/pers/yuanman/study/algorithm/binarytree/BinarTree.java
@@ -1,7 +1,6 @@
 package pers.yuanman.study.algorithm.binarytree;
 
 
-
 /**
  * 功能概要：排序/平衡二叉树
  * 二叉排序树的定义
@@ -11,6 +10,10 @@ public class BinarTree {
     public TreeNode root;
 
     public long size;
+    private int value;
+    private BinarTree left;
+
+    private BinarTree right;
 
     /**
      * 往树中加节点
@@ -62,18 +65,113 @@ public class BinarTree {
 
     /**
      * 删除树中节点
-     * @param data
-     * @return Boolean 插入成功返回true
+     * @param value
      */
-    public Boolean deteleTreeNode(Integer data){
-        //自己写 先删除左边的，然后再一起考虑右边的
-        return false;
+    public void deteleTreeNode(Integer value){
+
+            if (root == null) {
+                return;
+            } else {
+                TreeNode node = findTreeNode(value);
+                TreeNode parent = (TreeNode) searchParent(value);
+                //如果找不到要删除的结点，退出方法
+                if (node == null) {
+                    return;
+                }
+                //只有一个结点，该结点为根结点，则该结点一定会被删除
+                if (root.left == null && root.right == null) {
+                    root = null;
+                    return;
+                }
+                //如果要删除的结点为叶子结点
+                if (node.left == null && node.right == null) {
+                    //判断叶子结点为左子结点还是右子结点
+                    if (parent.left != null && parent.left.value == value) {
+                        parent.left = null;
+                        return;
+                    } else if (parent.right != null && parent.right.value == value) {
+                        parent.right = null;
+                        return;
+                    }
+                } else if (node.left != null && node.right != null) {
+//                要删除的结点有两颗子树
+//                找到要删除结点右子树的最小结点
+                    node.value = delRightMin(node.right);
+                } else {
+//                要删除的结点有一颗子树
+                    if (root.value == value) {
+//                    当删除结点为根结点
+                        if (root.left != null) {
+                            root = root.left;
+                        } else {
+                            root = root.right;
+                        }
+                    } else if (parent.left == node) {
+//                    删除结点为左子结点
+                        if (node.left != null) {
+//                       删除的结点有左子树
+                            parent.left = node.left;
+                        } else {
+//                        删除的结点有右子树
+                            parent.left = node.right;
+                        }
+                    } else {
+//                    删除的结点为右子结点
+                        if (node.left != null) {
+                            parent.right = node.left;
+                        } else {
+                            parent.right = node.right;
+                        }
+                    }
+                }
+            }
+        }
+
+    private Integer delRightMin(TreeNode right) {
+        return null;
     }
-    /**
-     * 查询
-     * @param data
-     * @return TreeNode
-     */
+
+
+    //查找结点，返回值为该结点，如果找不到，返回null
+        public BinarTree searchNode(int value) {
+            if (value == this.value) {
+                return this;
+            } else if (value < this.value) {
+                if (this.left == null) {
+                    return null;
+                }
+                return this.left.searchNode(value);
+            } else {
+                if (this.right == null) {
+                    return null;
+                }
+                return this.right.searchNode(value);
+            }
+        }
+
+        //查找要删除结点的父结点
+        public Object searchParent(int value) {
+            if ((this.left != null && this.left.value == value) || (this.right != null && this.right.value == value)) {
+                return this;
+            } else {
+                if (this.left != null && value < this.value) {
+                    return this.left.searchParent(value);
+                } else if (this.right != null && value >= this.value) {
+                    return this.right.searchParent(value);
+                } else {
+                    return null;
+                }
+            }
+        }
+
+
+
+
+        /**
+         * 查询
+         * @param data
+         * @return TreeNode
+         */
     public TreeNode findTreeNode(Integer data){
         if(null == root){
             return null;
