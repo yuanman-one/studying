4d725fc9fe4e5b4d4c846624774b9733005e182c
yuanman-one <1782271387@qq.com>
2022-05-18 23:46:17 +0800

    *添加第一个控制器

--- springbootdemo/src/main/java/com/example/springbootdemo/controller/FirstController.java
+++ springbootdemo/src/main/java/com/example/springbootdemo/controller/FirstController.java
@@ -2,19 +2,21 @@ package com.example.springbootdemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/first")
 @RestController
 @Slf4j
 public class FirstController {
 
-    @GetMapping("/test")
-    public String firstTest(String str){
-        log.info("记录日志打印在控制台，这是请求到的第一个控制器接口,接收的参数："+str);
-        return "这是请求的第一个控制器接口，接收的参数："+str;
+    @GetMapping("/test/get")
+    public String firstTestGet(String str){
+        log.info("记录日志打印在控制台，这是get请求到的第一个控制器接口,接收的参数："+str);
+        return "这是get请求的第一个控制器接口，接收的参数："+str;
+    }
+    @PostMapping("/test/post")
+    public String firstTestPost(String str){
+        log.info("记录日志打印在控制台，这是post请求到的第一个控制器接口,接收的参数："+str);
+        return "这是post请求的第一个控制器接口，接收的参数："+str;
     }
 }
